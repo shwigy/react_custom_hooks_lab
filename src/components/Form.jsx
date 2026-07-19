07f1320a2e43b18c55acd193ff567e8ba883e268
shwigy <floressj04@icloud.com>
2026-07-18 21:11:03 -0400

    added content to Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,30 @@
 import { useLocalStorage } from "../hooks/useLocalStorage";
+
 function Form() {
+  const [name, setName] = useLocalStorage("name", "");
+  const [service, setService] = useLocalStorage("service", "");
 
-    return (
-      <>
-        <form style={{ display: "flex", flexDirection: "column" }}>
-            <label htmlFor="name">Name:</label>
-            <input type="text" data-testid={"name"} />
-            <label htmlFor="service">Service Number:</label>
-            <input type="text" data-testid={"service"} />
-
-        </form>
-        <h4>{name ? `Welcome, ${name}!` : "Enter your name"}</h4>
-      </>
-    );
+  return (
+    <>
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          data-testid={"name"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <label htmlFor="service">Service Number:</label>
+        <input
+          type="text"
+          data-testid={"service"}
+          value={service}
+          onChange={(e) => setService(e.target.value)}
+        />
+      </form>
+      <h4>{name ? `Welcome, ${name}!` : "Enter your name"}</h4>
+    </>
+  );
 }
 
-export default Form
+export default Form;
